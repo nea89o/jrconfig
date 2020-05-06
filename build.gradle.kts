92358494319c66a5ded4ee13a173846cd15f284e
Roman Gräf <romangraef@gmail.com>
2020-05-06 12:28:54 +0200

    Better maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     java
     kotlin("jvm") version "1.3.72"
     id("com.github.johnrengelman.shadow") version "4.0.4"
+    maven
 }
 
 
@@ -14,6 +16,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testCompile("junit", "junit", "4.12")
 }
+
+artifacts {
+    archives(tasks.kotlinSourcesJar)
+}
+
 tasks {
     named<ShadowJar>("shadowJar") {
         archiveBaseName.set("jrconfig-shadow")
